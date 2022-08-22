42b41e83be0300f84117304a1e6a6b40494b3324
Glitch (glitch-hello-react) <none>
2022-08-22 05:26:50 +0000

    ⛈👑 Checkpoint ./src/components/footer/footer.module.css:47879979/6 ./src/components/footer/Footer.jsx:47879979/3367

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import FooterStyle from "./footer.module.css";
+
+export default function Footer() {
+  return (
+<>
+    <p>
+    hy</p>
+    </>
+  );
+}
