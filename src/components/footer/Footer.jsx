a63b5e3cbbe87281b2a935a333219931631100c7
Glitch (glitch-hello-react) <none>
2022-08-22 06:25:10 +0000

    🏃👝 Checkpoint ./src/components/footer/Footer.jsx:47313412/4777

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,40 +3,56 @@ import FooterStyle from "./footer.module.css";
 
 export default function Footer() {
   return (
-<>
-  <footer class={FooterStyle.footerClass}>
-  kerala
-  </footer>
-</>
+    <>
+      <footer class={FooterStyle.footerClass}>
+        <div class={FooterStyle.mainDiv1}>
+          <div class={FooterStyle.sub1MainDiv1}>
+            <p>Subribe Us</p>
+            <form class={FooterStyle.formClassSub1}>
+              <input type="email" required></input>
+              <button>Submit</button>
+            </form>
+          </div>
+
+          <div class={FooterStyle.sub2MainDiv1}>
+            <div class={FooterStyle.sub2P1MainDiv1}>
+              <img src="#" alt="logo" />
+              <p>Make some awesome blogs!</p>
+            </div>
+
+            <div class={FooterStyle.sub2P2MainDiv1}>
+              <ul>
+                <li>About</li>
+                <li>Jobs</li>
+                <li>Docs</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class={FooterStyle.mainDiv2}>
+          <div class={FooterStyle.sub1MainDiv2}>
+            <img src="#" alt="sailing pic" />
+          </div>
+
+          <div class={FooterStyle.sub2MainDiv2}>
+            <div class={FooterStyle.sub2P1MainDiv2}>
+              <ul>
+                <li>Terms and Conditions</li>
+                <li>Privacy Policy</li>
+                <li>Cookie Policy</li>
+              </ul>
+            </div>
+            <div class={FooterStyle.sub2P2MainDiv2}>
+              <div class={FooterStyle.sub2P2s1MainDiv2}>
+                <h6>Let's chat!</h6>
+                <p>mailtosreesankar@gmail.com</p>
+                <div class={FooterStyle.logoCahin}></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
-<div></div>
-    
-    
-    ,<div></div>
-    
-     class={}mainFooterStyle.mainDiv1 class={FooterSty
-    le.mainDiv1}32
-    <div></div>
-    di
-    v<></div> class={FooterStyle.sub1MainDiv1}
-
-      <p>Subribe Us</p>
-      <form class={FooterStyle.formClassSub1}>
-       <input type="email" required></input>
-       <button>Submit</button>
-      </form>    
-    
- 
-    
-        class={FooterStyle.sub1MainD2v1}
-      
-      <div></div> class={FooterStyle.sub2P1MainDiv1}
-         <img src="#" alt="logo" />
-        <p>Make some awesome blogs!</p>      
-      
-       <div cass={FooterStyle.sub2P1MainDi21}>
-      
-      <ul>
-        <li></li>
-        </ul>ul</div>
