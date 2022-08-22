975c19339c932396603f8c3d4c5e6e181a55d1ce
Glitch (glitch-hello-react) <none>
2022-08-22 06:13:53 +0000

    🎪🎉 Checkpoint ./src/components/footer/footer.module.css:47313412/24 ./src/components/footer/Footer.jsx:47313412/994

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,3 +10,33 @@ export default function Footer() {
 </>
   );
 }
+<div></div>
+    
+    
+    ,<div></div>
+    
+     class={}mainFooterStyle.mainDiv1 class={FooterSty
+    le.mainDiv1}32
+    <div></div>
+    di
+    v<></div> class={FooterStyle.sub1MainDiv1}
+
+      <p>Subribe Us</p>
+      <form class={FooterStyle.formClassSub1}>
+       <input type="email" required></input>
+       <button>Submit</button>
+      </form>    
+    
+ 
+    
+        class={FooterStyle.sub1MainD2v1}
+      
+      <div></div> class={FooterStyle.sub2P1MainDiv1}
+         <img src="#" alt="logo" />
+        <p>Make some awesome blogs!</p>      
+      
+       <div cass={FooterStyle.sub2P1MainDi21}>
+      
+      <ul>
+        <li></li>
+        </ul>ul</div>
