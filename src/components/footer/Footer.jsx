3c8d31ba184675afc70fe28dfd75f7386a8dfda5
Glitch (glitch-hello-react) <none>
2022-08-23 03:45:42 +0000

    🌎⛺️ Checkpoint ./src/components/footer/Footer.jsx:47313412/1133

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,11 @@ import { AiFillFacebook, AiFillTwitterSquare, AiFillLinkedin, AiFillInstagram } 
 export default function Footer() {
   return (
     <>
+      {/* 1 */}
       <footer class={FooterStyle.footerClass}>
+        {/* 2 */}
         <div class={FooterStyle.mainDiv1}>
+          {/* 3 */}
           <div class={FooterStyle.sub1MainDiv1}>
             <form>
              <p>Subscribe Us</p>
@@ -14,13 +17,16 @@ export default function Footer() {
               <button>Submit</button>
             </form>
           </div>
-
+          {/* 3 closes */}
+          {/* 4 */}
           <div class={FooterStyle.sub2MainDiv1}>
+          {/* 5 */}
             <div class={FooterStyle.sub2P1MainDiv1}>
               <img class={FooterStyle.logoChange} src="https://cdn.glitch.global/1f2e8776-0d5d-40c2-b754-4ea7e29b14c7/logorem.png" alt="logo" />
               <p class={FooterStyle.paraG}>Make some awesome blogs!</p>
             </div>
-
+          {/* 5 closes */}
+          {/* 6 */}
             <div class={FooterStyle.sub2P2MainDiv1}>
               <ul>
                 <li>About</li>
@@ -28,15 +34,23 @@ export default function Footer() {
                 <li>Docs</li>
               </ul>
             </div>
+            {/* 6 closes */}
           </div>
+          {/* 4 closes */}
         </div>
+        {/* 2 closes */}
 
+        
+        {/* 7 */}
         <div class={FooterStyle.mainDiv2}>
+          {/* 8 */}
           <div class={FooterStyle.sub1MainDiv2}>
             <img src="https://cdn.glitch.global/1f2e8776-0d5d-40c2-b754-4ea7e29b14c7/shipremove.png" alt="sailing pic" class={FooterStyle.posterChange} />
           </div>
-
+          {/* 8 closes */}
+          {/* 9 */}
           <div class={FooterStyle.sub2MainDiv2}>
+            {/* 10 */}
             <div class={FooterStyle.sub2P1MainDiv2}>
               <ul>
                 <li>Terms and Conditions</li>
@@ -44,10 +58,14 @@ export default function Footer() {
                 <li>Cookie Policy</li>
               </ul>
             </div>
+            {/* 10 closes */}
+            {/* 11 */}
             <div class={FooterStyle.sub2P2MainDiv2}>
+              {/* 12 */}
               <div class={FooterStyle.sub2P2s1MainDiv2}>
                 <h6 class={FooterStyle.h6}>Let's chat!</h6>
                 <p class={FooterStyle.p}>email us</p>
+                {/* 13 closes */}
                 <div class={ FooterStyle.logosP}>
                   <AiFillFacebook style={{fontSize:"18px"}} />
                   <AiFillTwitterSquare style={{fontSize:"18px"}} />
@@ -55,9 +73,13 @@ export default function Footer() {
                   <AiFillInstagram style={{fontSize:"18px"}}/>
                 </div>
               </div>
+              {/* 12 closes */}
             </div>
+            {/* 11 closes */}
           </div>
+          {/* 9 closes */}
         </div>
+        {/* 7 closes */}
       </footer>
     </>
   );
